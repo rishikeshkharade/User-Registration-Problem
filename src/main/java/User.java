c0704d993a378be4b50308938684c55084a31e0b
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2024-12-06 16:57:59 +0530

    UC2 Enter Valid Last Name

--- src/main/java/User.java
+++ src/main/java/User.java
@@ -5,6 +5,10 @@ public class User {
         String regex = "^[A-Z][a-zA-Z]{2,}$";
         return Pattern.matches(regex, firstName);
     }
+    public static boolean isValidLastName(String lastName){
+        String regex = "^[A-Z][a-zA-Z]{2,}$";
+        return Pattern.matches(regex, lastName);
+    }
 
     public static void main(String[] args) {
         System.out.println("Welcome to User Registration Problem");
@@ -15,6 +19,13 @@ public class User {
             System.out.println("Valid first name.");
         } else {
             System.out.println("Invalid first name. It must start with a capital letter and have at least 3 characters.");
+        }
+        System.out.println("Enter the Last Name");
+        String lastName = sc.nextLine();
+        if (isValidLastName(lastName)){
+            System.out.println("Valid Last name.");
+        }else {
+            System.out.println("Invalid last name. It must start with a capital letter and have at least 3 characters.");
         }
 
     }
